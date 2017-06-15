c9c8d3a97317a4351f899052fedef16cb9598d60
PowerShell Usergroup Hannover <info@psugh.de>
2017-06-15 22:46:55 +0200

    Create Get-PSUGH.ps1

--- Get-PSUGH.ps1
+++ Get-PSUGH.ps1
@@ -1,5 +1,26 @@
-$json = ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri 'https://psugh.github.io/data.json')
-$out = @'
+function Get-PSUGH
+{
+  <#
+    .SYNOPSIS
+    Get-PSUGH shows the next appointment for PowerShell Usergroup 
+    Hannover meeting
+
+    .DESCRIPTION
+    Get the next appointment for PowerShell Usergroup Hannover
+    meeting in your PowerShell terminal. That's all.
+
+    .EXAMPLE
+    Get-PSUGH
+    Shows the next appointment for PowerShell Usergroup Hannover
+    meeting
+
+    .LINK
+    http://www.psugh.de
+
+  #>
+
+  $json = ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri 'https://psugh.github.io/data.json')
+  $out = @'
 
 {0}
 {1}
@@ -10,5 +31,8 @@ Das ist {5} in {6}.
 Mit folgenden Themen: 
 '@ -f $json.Treffen, $('-' * $($json.Treffen).Length), $json.Datum, $json.Zeit, $json.Ort, $json.Strasse, $json.Stadt
 
-$out
-$json.Themen
+  $out
+  $json.Themen
+}
+
+Get-PSUGH
