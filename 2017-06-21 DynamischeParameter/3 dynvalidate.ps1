94aca8d4b1c31c1efc850c2afa884e4bd85dcb72
Christian Imhorst <christian.imhorst@gmail.com>
2017-08-28 20:59:49 +0200

    Create 3 dynvalidate.ps1

---
+++ 2017-06-21 DynamischeParameter/3 dynvalidate.ps1
@@ -0,0 +1,51 @@
+function Test3
+{
+
+  [CmdletBinding()]
+  param
+  (
+    [ValidateSet('great','ok','soso','puke')]
+    [string]
+    $EnumStatic
+  )
+
+  dynamicparam
+  {
+    #Start-Sleep -Milliseconds 1000
+    # create a validate set dynamically (result is unpredictable - lesson: caching!)
+    $cities = 'Hannover', 'Braunschweig', 'NewYork', 'London', 'Paris' | Get-Random -Count 2
+    # create a dictionary that stores all dynamic parameters:
+    $paramDictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+    
+    # create an attribute collection to add all attributes needed to the dynamic parameter:
+    $attributeCollection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+
+    # create attribute "ParameterSetName" and make this parameter visible in all parametersets:
+    # (add more of these attributes to define more than one parameterset, and add them all to the attribute collection)
+    $attributes = New-Object System.Management.Automation.ParameterAttribute
+    $attributes.ParameterSetName = '__AllParameterSets'
+    $attributes.Mandatory = $true
+    # add attribute to collection:
+    $attributeCollection.Add($attributes)
+
+    $Name = 'EnumDynamic'
+    $arrSet = $cities
+    $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+    $attributeCollection.Add($ValidateSetAttribute)
+    $dynParam = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($Name, [string] , $attributeCollection)
+
+    # add new dynamic parameter to the list of available dynamic parameters:
+    $paramDictionary.Add($Name, $dynParam)
+    $paramDictionary
+  }
+
+  # once scriptblock content is assigned to blocks, all content must be assigned to appropriate blocks
+  end
+  {
+    # dynamic parameters surface via $PSBoundParameters
+    Test-Path variable:ForceStatic
+    Test-Path variable:ForceDynamic
+    $PSBoundParameters
+
+  }
+}
