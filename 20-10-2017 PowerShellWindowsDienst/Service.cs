a33963b90d57d1e0a789510e5d4bae03c0bbac83
Christian Imhorst <christian.imhorst@gmail.com>
2017-10-20 20:53:55 +0200

    Create Service.cs

---
+++ 20-10-2017 PowerShellWindowsDienst/Service.cs
@@ -0,0 +1,53 @@
+using System.ServiceProcess;
+using System.ServiceModel;
+using System.Configuration.Install;
+using System.ComponentModel;
+
+// Das Grundgerüst des Dienstes:
+
+namespace Dienst
+{
+    public class Service : ServiceBase
+    {
+        static void Main()
+        {
+            Service.Run(new Service());
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+
+    // Der Installer des Dienstes:
+    [RunInstaller(true)]
+    public class Installation : Installer
+    {
+        private ServiceInstaller service;
+        private ServiceProcessInstaller process;
+
+        public Installation()
+        {
+            service = new ServiceInstaller();
+            process = new ServiceProcessInstaller();
+
+            service.ServiceName = "MeinWindowsDienst";
+            service.DisplayName = "MeinWindowsDienst";
+            service.Description = "Mein Windows-Dienst";
+
+            process.Account = ServiceAccount.LocalSystem;
+
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
